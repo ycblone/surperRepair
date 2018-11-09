511982742aa9a4fd91b11b337e2121005cbb6202
Wevan <438864121@qq.com>
2018-11-09 18:40:15 +0800

    Adding files in project 'cordovaApp' to Git...

---
+++ RepairVue/src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+import HelloWorld from '@/components/HelloWorld'
+import Test from "@/components/Test";
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Test',
+      component: Test
+    }
+  ]
+})
