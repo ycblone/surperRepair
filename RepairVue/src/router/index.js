6c5ea5001738bf7dff79c3ab1f1d2182da90da95
Wevan <438864121@qq.com>
2018-11-20 17:07:14 +0800

    :bug: 修复 BUG
    - 删除路由中无用引用

--- RepairVue/src/router/index.js
+++ RepairVue/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HelloWorld from '@/components/HelloWorld'
 import Register from '@/components/Register'
 import Index from '@/components/Index'
 import Login from "@/components/Login";
