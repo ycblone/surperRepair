6cfca837bd8e8e798866130b789a39829b642e06
Wevan <438864121@qq.com>
2018-11-20 16:51:59 +0800

    :construction_worker: 添加 CI 构建到系统
    - 测试ci

--- RepairVue/src/components/Index.vue
+++ RepairVue/src/components/Index.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="content">
-    <van-nav-bar
-      title="物业端"
-    />
-<div style="margin-top: 50px">
-  <van-row style="margin-top: 30px">
-    <van-col span="8"><van-icon name="info-o" size="40px"/>
-      <div>维修管理</div>
-    </van-col>
-    <van-col span="8"><van-icon name="success" size="40px" /></van-col>
-    <van-col span="8"><van-icon name="success" size="40px" /></van-col>
-  </van-row>
-  <van-row style="margin-top: 30px">
-    <van-col span="8"><van-icon name="success" size="40px" /></van-col>
-    <van-col span="8"><van-icon name="success" size="40px" /></van-col>
-    <van-col span="8"><van-icon name="success" size="40px" /></van-col>
-  </van-row>
-  <van-row style="margin-top: 30px">
-    <van-col span="8"><van-icon name="success" size="40px" /></van-col>
-    <van-col span="8"><van-icon name="success" size="40px" /></van-col>
-    <van-col span="8"><van-icon name="success" size="40px" /></van-col>
-  </van-row>
-</div>
+    <van-nav-bar title="物业端"/>
+    <div style="margin-top: 50px">
+      <van-row style="margin-top: 30px">
+        <van-col span="8">
+          <van-icon name="info-o" size="40px"/>
+          <div>维修管理</div>
+        </van-col>
+        <van-col span="8">
+          <van-icon name="success" size="40px"/>
+        </van-col>
+        <van-col span="8">
+          <van-icon name="success" size="40px"/>
+        </van-col>
+      </van-row>
+      <van-row style="margin-top: 30px">
+        <van-col span="8">
+          <van-icon name="success" size="40px"/>
+        </van-col>
+        <van-col span="8">
+          <van-icon name="success" size="40px"/>
+        </van-col>
+        <van-col span="8">
+          <van-icon name="success" size="40px"/>
+        </van-col>
+      </van-row>
+      <van-row style="margin-top: 30px">
+        <van-col span="8">
+          <van-icon name="success" size="40px"/>
+        </van-col>
+        <van-col span="8">
+          <van-icon name="success" size="40px"/>
+        </van-col>
+        <van-col span="8">
+          <van-icon name="success" size="40px"/>
+        </van-col>
+      </van-row>
+    </div>
 
   </div>
 
